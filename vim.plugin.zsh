0c7d26f7caa4f65f2d8272c14e23dca0c69ad960
CHAN JIAN LE <49647142+BruceChanJianLe@users.noreply.github.com>
2023-06-05 16:35:52 +0800

    wip: text obj and vim surround

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -34,3 +34,27 @@ preexec() { echo -ne '\e[1 q' ;} # Use beam shape cursor for each new prompt.
 # emacs like keybindings
 bindkey -M viins '^A' beginning-of-line 
 bindkey -M viins '^E' end-of-line 
+
+# Text Object
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+  bindkey -M $km -- '-' vi-up-line-or-history
+  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+    bindkey -M $km $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $km $c select-bracketed
+  done
+done
+
+# Vim Surround
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
+bindkey -M visual S add-surround
